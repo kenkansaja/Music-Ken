9861e466407650f7b9f411cf4b205ef31a24655f
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-07 20:16:48 +0700

    Update private.py

--- MusicKen/modules/private.py
+++ MusicKen/modules/private.py
@@ -38,7 +38,7 @@ Saya Memiliki Banyak Fitur Praktis Seperti :
 ┣• Gunakan Perintah » /help « untuk Mengetahui Fitur Lengkap saya
 ┗━━━━━━━━━━━━━━
 ❃ Managed With ❤ By {OWNER}
-❃ Thanks To [Risman](https://t.me/kenkanasw)
+❃ Thanks To [KenKan](https://t.me/kenkanasw)
 ━━━━━━━━━━━━━━━
 Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
 </b>""",
