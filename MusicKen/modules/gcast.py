0ed614e4469a50c211955d83227c2b206af898d7
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-11 09:45:29 +0700

    Update gcast.py

--- MusicKen/modules/gcast.py
+++ MusicKen/modules/gcast.py
@@ -1,23 +1,26 @@
-from MusicKen.config import que
+
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+import asyncio
 from MusicKen.config import SUDO_USERS
 
-
-@que(outgoing=True, pattern="^.gcast (.*)")
-async def gcast(event):
-    xx = event.pattern_match.group(1)
-    if not xx:
-        return await config.edit("`Mohon Berikan Sebuah Pesan`")
-    tt = event.text
-    msg = tt[6:]
-    kk = await config.edit("`Sedang Mengirim Pesan Secara Global... 📢`")
-    er = 0
-    done = 0
-    async for x in bot.iter_dialogs():
-        if x.is_group:
-            chat = x.id
+@Client.on_message(filters.command(["gcast"]))
+async def bye(client, message):
+    sent=0
+    failed=0
+    if message.from_user.id in SUDO_USERS:
+        lol = await message.reply("`Sedang mengirim pesan global...`")
+        if not message.reply_to_message:
+            await lol.edit("**Balas pesan teks apa pun untuk gcast**")
+            return
+        msg = message.reply_to_message.text
+        for dialog in client.iter_dialogs():
             try:
-                done += 1
-                await bot.send_message(chat, msg)
-            except BaseException:
-                er += 1
-    await kk.edit(f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**")
+                await client.send_message(dialog.chat.id, msg)
+                sent = sent+1
+                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+            except:
+                failed=failed+1
+                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+            await asyncio.sleep(0.7)
+        await message.reply_text(f"**Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
