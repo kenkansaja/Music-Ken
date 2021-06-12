613b711402d97b6270194bb32213beabc78fbbc3
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-12 22:42:28 +0700

    Update config.py

--- MusicKen/config.py
+++ MusicKen/config.py
@@ -1,20 +1,3 @@
-# DAISYXMUSIC- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
 import os
 from os import getenv
 
@@ -43,6 +26,5 @@ ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-STICKERS = getenv("STICKERS", "aHR0cHM6Ly90Lm1lL2FkZHN0aWNrZXJzL0ZyaWVuZGx5X1BhbmRh")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
