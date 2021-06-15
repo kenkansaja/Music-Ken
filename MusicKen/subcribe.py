55f8dd6ea2ae784e10c1138e436c02c636bc5717
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-06-15 18:42:16 +0700

    Update subcribe.py

--- MusicKen/subcribe.py
+++ MusicKen/subcribe.py
@@ -1,18 +1,18 @@
 import re
 import asyncio
 from pyrogram import filters
-from config import UPDATES_CHANNEL, ASSISTANT_NAME
+from config import SUPPORT_GROUP, ASSISTANT_NAME
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
 async def is_subscribed(filter, client, message):
-    if not UPDATES_CHANNEL:
+    if not SUPPORT_GROUP:
         return True
     user_id = message.from_user.id
     if user_id in ASSISTANT_NAME:
         return True
     try:
-        member = await client.get_chat_member(chat_id = UPDATES_CHANNEL, user_id = user_id)
+        member = await client.get_chat_member(chat_id = SUPPORT_GROUP, user_id = user_id)
     except UserNotParticipant:
         return False
 
