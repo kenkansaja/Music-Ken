bdee22c8697e1b53a23b6c53c76f37bf2cd2bb87
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-06 09:46:50 +0700

    Add files via upload

--- MusicKen/function/__init__.py
+++ MusicKen/function/__init__.py
@@ -1,3 +1,5 @@
-from MusicKen.function.admins import admins, get, set
+from MusicKen.function.admins import admins
+from MusicKen.function.admins import get
+from MusicKen.function.admins import set
 
 __all__ = ["set", "get", "admins"]
