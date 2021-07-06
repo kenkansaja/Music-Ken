6c0fcfd20e91510b02bfa245a3b66c28a4381b19
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-06 11:07:48 +0700

    Update and rename MusicKen/services/queues/__init__.py to MusicKen/services/callsmusic/queues/__init__.py

--- MusicKen/services/queues/__init__.py
+++
@@ -1,7 +0,0 @@
-from MusicKen.services.queues.queues import clear 
-from MusicKen.services.queues.queues import get
-from MusicKen.services.queues.queues import is_empty
-from MusicKen.services.queues.queues import put
-from MusicKen.services.queues.queues import task_done
-
-__all__ = ["clear", "get", "is_empty", "put", "task_done"]
