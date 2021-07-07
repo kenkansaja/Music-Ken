b305427453f3a72f7fba97fb2ba1d4664333f273
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-07 19:37:17 +0700

    Update play.py

--- MusicKen/modules/play.py
+++ MusicKen/modules/play.py
@@ -342,7 +342,7 @@ async def m_cb(b, cb):
             await cb.answer("Chat is not connected!", show_alert=True)
 
 
-@Client.on_message(command("play", "yplay") & other_filters)
+@Client.on_message(command("play") & other_filters)
 async def play(_, message: Message):
     global que
     lel = await message.reply("🔄 **Sedang Memproses Lagu**")
