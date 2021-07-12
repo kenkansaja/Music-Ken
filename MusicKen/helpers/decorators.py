ef893c2c966aa1efc1a6d8d36edfc35300068055
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-12 17:46:54 +0700

    Update decorators.py

--- MusicKen/helpers/decorators.py
+++ MusicKen/helpers/decorators.py
@@ -32,27 +32,26 @@ def authorized_users_only(func: Callable) -> Callable:
     return decorator
 
 
-def subcribe(func: Callable) -> Callable:
-     async def decorator(client: Client, message: Message):
-
-         if not SUPPORT_GROUP:
-            return True
-            user_id = message.from_user.id
-         if user_id in SUDO_USERS:
-            return True
-         try:
-            member = await client.get_chat_member(chat_id = SUPPORT_GROUP, user_id = user_id)
-            except UserNotParticipant:
-            return await message.reply("""**Anda harus bergabung dulu di group kami kak untuk bisa menggunakan bot ini**""",
-              reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                "💬 GROUP", url=f"https://t.me/{SUPPORT_GROUP}"
-                            ),
-                            InlineKeyboardButton(
-                                "OWNER 👮", url=f"https://t.me/kenkanasw"
-                            )
-                        ]
-                    ]
-            return decorator
+def subscribe(func: Callable) -> Callable:
+  async def decorator(client: Client, message: Message):
+    if not SUPPORT_GROUP:
+      return True
+      user_id=message.from_user.id
+    if user_id in SUDO_USERS:
+      return True
+      try:
+        member=await client.get_chat_member(chat_id = SUPPORT_GROUP, user_id = user_id)
+        except UserNotParticipant:
+          return await message.reply("""**Anda harus bergabung dulu di group kami kak untuk bisa menggunakan bot ini**""",
+                        reply_markup=InlineKeyboardMarkup(
+                              [
+                                  [
+                                      InlineKeyboardButton(
+                                          "💬 GROUP", url=f"https://t.me/{SUPPORT_GROUP}"
+                                      ),
+                                      InlineKeyboardButton(
+                                          "OWNER 👮", url=f"https://t.me/kenkanasw"
+                                      )
+                                  ]
+                              ]
+                      return decorator
