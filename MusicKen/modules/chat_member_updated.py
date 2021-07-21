9abc7ec20ebf480406f035c5d1aa172d75f85556
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-22 05:22:17 +0700

    Update chat_member_updated.py

--- MusicKen/modules/chat_member_updated.py
+++ MusicKen/modules/chat_member_updated.py
@@ -1,31 +1,17 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from MusicKen.function import *
+from MusicKen.function import admin as cache
 
 
 @Client.on_chat_member_updated()
 async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
-    if chat_member_updated.new_chat_member \
-            and chat_member_updated.old_chat_member:
-        (
-            admins.admins[chat_member_updated.chat.id].append(
-                chat_member_updated.new_chat_member.user.id,
-            )
-        ) if (
-            (
-                chat_member_updated.new_chat_member.can_manage_voice_chats
-            ) and (
-                (
-                    chat_member_updated.new_chat_member.user.id
-                ) not in admins.admins[chat_member_updated.chat.id]
-            )
-        ) else (
-            admins.admins[chat_member_updated.chat.id].remove(
-                chat_member_updated.new_chat_member.user.id,
-            )
-        ) if (
-            (
-                chat_member_updated.new_chat_member.user.id
-            ) in admins.admins[chat_member_updated.chat.id]
-        ) else None
+    chat = chat_member_updated.chat.id
+    new = chat_member_updated.new_chat_member
+
+    if new.can_manage_voice_chats:
+        if new.user.id not in cache.admins[chat]:
+            cache.admins[chat].append(new.user.id)
+    else:
+        if new.user.id in cache.admins[chat]:
+            cache.admins[chat].remove(new.user.id)
