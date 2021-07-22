6a9826a8beec3e7de833ce877da1ccb42dc19377
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-22 18:58:32 +0700

    Update chat_member_updated.py

--- MusicKen/modules/chat_member_updated.py
+++ MusicKen/modules/chat_member_updated.py
@@ -6,13 +6,13 @@ from MusicKen.function import *
 async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
   if chat_member_updated.new_chat_member \
   and chat_member_updated.old_chat_member:
-    (admins.admins[chat_member_updated.chat.id].append(chat_member_updated.new_chat_member.user.id))
+    (admins.admins[chat_member_updated.chat.id].append(chat_member_updated.new_chat_member.user.id,))
       if 
       (chat_member_updated.new_chat_member.can_manage_voice_chats)
         and
         ((chat_member_updated.new_chat_member.user.id) not in admins.admins[chat_member_updated.chat.id])
           else
-          (admins.admins[chat_member_updated.chat.id].remove(chat_member_updated.new_chat_member.user.id))
+          (admins.admins[chat_member_updated.chat.id].remove(chat_member_updated.new_chat_member.user.id,))
             if
             ((chat_member_updated.new_chat_member.user.id) in admins.admins[chat_member_updated.chat.id])
               else None
