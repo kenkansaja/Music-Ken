63fda89d78bae8e2a4efd9f43072f7c9682153d9
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-23 12:01:39 +0700

    Update gcast.py

--- MusicKen/modules/gcast.py
+++ MusicKen/modules/gcast.py
@@ -1,25 +1,30 @@
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
 import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Dialog, Chat, Message
+from pyrogram.errors import UserAlreadyParticipant
 from MusicKen.config import SUDO_USERS
+from MusicKen.helpers.filters import command
+from MusicKen.services.callsmusic.callsmusic import client as USER
 
-@Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+
+@Client.on_message(command("gcast") & filters.user(SUDO_USERS) & ~filters.edited)
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("`Sedang mengirim pesan global...`")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Sedang mengirim pesan global...`")
         if not message.reply_to_message:
-            await lol.edit("**Balas pesan teks apa pun untuk gcast**")
+            await wtf.edit("`Balas pesan teks apa pun untuk gcast`")
             return
-        msg = message.reply_to_message.text
-        for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in USER.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await wtf.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
-            await asyncio.sleep(0.7)
-        await message.reply_text(f"**Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await message.reply_text(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
