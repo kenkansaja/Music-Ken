67ad42f15f19bcbde132beec7115e588a1a5c0b3
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-24 13:33:07 +0700

    Update gcast.py

--- MusicKen/modules/gcast.py
+++ MusicKen/modules/gcast.py
@@ -6,10 +6,6 @@ from MusicKen.config import SUDO_USERS
 from MusicKen.helpers.filters import command
 from MusicKen.services.callsmusic.callsmusic import client as USER
 
-
-
-
-
 @Client.on_message(command("gcast") & filters.user(SUDO_USERS) & ~filters.edited)
 async def gcast(_, message: Message):
     sent=0
@@ -27,8 +23,11 @@ async def gcast(_, message: Message):
                 await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
                 await wtf.edit(f"`Sedang mengirim pesan global......` \n\n**Terkirim ke:** `{sent}` chat \n**Gagal terkirim ke:** {failed} chat")
-                await asyncio.sleep(3)
+                await asyncio.sleep(0.7)
             except:
                 failed=failed+1
                 await wtf.edit(f"`Sedang mengirim pesan global` \n\n**Terkirim ke:** `{sent}` Chats \n**Gagal terkirim ke:** {failed} Chats")
-                await wtf.edit(f"`Pesan global selesai` \n\n**Terkirim ke:** `{sent}` Chats \n**Gagal terkirim ke:** {failed} Chats")
+                await asyncio.sleep(0.7)
+
+        await message.reply_text(f"`Pesan global selesai` \n\n**Terkirim ke:** `{sent}` Chats \n**Gagal terkirim ke:** {failed} Chats")
+                
