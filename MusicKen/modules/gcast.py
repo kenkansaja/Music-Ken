58fae1a27a7346da0869bf20e43dce5b30c21204
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-24 13:01:20 +0700

    Update gcast.py

--- MusicKen/modules/gcast.py
+++ MusicKen/modules/gcast.py
@@ -8,24 +8,26 @@ from MusicKen.services.callsmusic.callsmusic import client as USER
 
 
 
+
+
 @Client.on_message(command("gcast") & filters.user(SUDO_USERS) & ~filters.edited)
-async def broadcast(_, message: Message):
+async def gcast(_, message: Message):
     sent=0
     failed=0
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Sedang mengirim pesan global...`")
+        wtf = await message.reply("Sedang mengirim pesan global...")
         if not message.reply_to_message:
-            await wtf.edit("`Balas pesan teks apa pun untuk gcast`")
+            await wtf.edit("Balas pesan teks apa pun untuk gcast")
             return
         lmao = message.reply_to_message.text
         async for dialog in USER.iter_dialogs():
             try:
                 await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await wtf.edit(f"`Sedang mengirim pesan global......` \n\n**Terkirim ke:** `{sent}` chat \n**Gagal terkirim ke:** {failed} chat")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await wtf.edit(f"**Berhasil Mengirim Pesan Ke** `{sent}` **Grup, Gagal Mengirim Pesan Ke** `{failed}` **Grup**")
+                await message.reply_text(f"`Pesan global selesai` \n\n**Terkirim ke:** `{sent}` Chats \n**Gagal terkirim ke:** {failed} Chats")
