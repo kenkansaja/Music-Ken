870c94ca4514c5ed0a6c198857f2a88ac5bd50cd
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-08 22:20:24 +0700

    Update play.py

--- MusicKen/modules/play.py
+++ MusicKen/modules/play.py
@@ -227,7 +227,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "helper"
+        user.first_name = ""
     usar = user
     wew = usar.id
     try:
