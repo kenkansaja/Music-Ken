506bcc276230b714a89cfd2ee6637bb977426c3c
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-11 13:58:56 +0700

    Create ping.py

---
+++ MusicKen/modules/ping.py
@@ -0,0 +1,57 @@
+
+import aiohttp
+import math
+import os
+import requests
+import asyncio
+from MusicKen.config import BOT_USERNAME
+from time import time
+from datetime import datetime
+from helpers.filters import command
+from pyrogram import Client, filters, emoji
+from pyrogram.types import Message
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+
+@Client.on_message(command(["pn", f"pn@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        f"{emoji.PING_PONG} **PONG!!**\n"
+        f"`{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(command(["up", f"up@{BOT_USERNAME}"]) & ~filters.edited)
+async def get_uptime(client: Client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        f"{emoji.ROBOT}\n"
+        f"• **Uptime:** `{uptime}`\n"
+        f"• **Start Time:** `{START_TIME_ISO}`"
+    )
