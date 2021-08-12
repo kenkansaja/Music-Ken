827163db212e3fec85859d36a9732cba79433902
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-12 13:28:09 +0700

    Update kan.py

--- MusicKen/modules/kan.py
+++ MusicKen/modules/kan.py
@@ -442,7 +442,7 @@ async def play(_, message: Message):
                     )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/ab13882bb05849b6ba170.jpg"
+        thumb_name = "https://telegra.ph/file/bfc7198787440999409bc.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
