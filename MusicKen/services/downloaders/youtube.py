b4d060946bc1321bfd690859eae5d42a7a6949d7
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-15 05:55:15 +0700

    Update youtube.py

--- MusicKen/services/downloaders/youtube.py
+++ MusicKen/services/downloaders/youtube.py
@@ -21,12 +21,12 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"❌ Video yang berdurasi lebih dari {DURATION_LIMIT} menit tidak diizinkan, video yang disediakan berdurasi {duration} menit"
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
+            f"❌ Video yang berdurasi lebih dari {DURATION_LIMIT} menit tidak diizinkan, video yang disediakan berdurasi {duration} menit"
+      )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
