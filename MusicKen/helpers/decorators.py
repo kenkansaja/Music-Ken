c1cc40ff8beedbeca31b1f3044067e581a17d902
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-14 19:49:20 +0700

    Update decorators.py

--- MusicKen/helpers/decorators.py
+++ MusicKen/helpers/decorators.py
@@ -28,4 +28,9 @@ def authorized_users_only(func: Callable) -> Callable:
 
     return decorator
 
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
 
+    return decorator
