438fd2d254c64c49ac490963723a5535c946835d
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-08-23 07:06:31 +0700

    Update play.py

--- MusicKen/modules/play.py
+++ MusicKen/modules/play.py
@@ -381,7 +381,7 @@ async def m_cb(b, cb):
             await cb.answer("Chat is not connected!", show_alert=True)
 
 
-@Client.on_message(command(["play", "ytplay", "yt", "p"]) & other_filters)
+@Client.on_message(command(["ytplay", "yt", "p"]) & other_filters)
 @errors
 async def play(_, message: Message):
     global que
