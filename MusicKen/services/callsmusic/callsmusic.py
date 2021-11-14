be1d33cf4f663899720ba344e6ee2952f8edbc2a
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-11-14 20:11:18 +0700

    Update callsmusic.py

--- MusicKen/services/callsmusic/callsmusic.py
+++ MusicKen/services/callsmusic/callsmusic.py
@@ -9,7 +9,7 @@ pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-def on_stream_end(chat_id: int) -> None:
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
