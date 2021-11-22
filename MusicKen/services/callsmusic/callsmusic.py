28de117b957f4df737ffbdb4d346caaf33b99f8d
kenkansaja <kenkanwardana@gmail.com>
2021-11-22 13:36:54 +0000

    Auto Fixes

--- MusicKen/services/callsmusic/callsmusic.py
+++ MusicKen/services/callsmusic/callsmusic.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import InputAudioStream
-from pytgcalls.types.input_stream import InputStream
+from pytgcalls.types.input_stream import InputAudioStream, InputStream
+
 from MusicKen.config import API_HASH, API_ID, SESSION_NAME
 from MusicKen.services.callsmusic import queues
 
@@ -18,7 +18,7 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
         await pytgcalls.leave_group_call(chat_id)
     else:
         await pytgcalls.change_stream(
-            chat_id, 
+            chat_id,
             InputStream(
                 InputAudioStream(
                     queues.get(chat_id)["file"],
