811ced20e7ef52a366612409e6c5544b13f4e60f
kenkansaja <kenkanwardana@gmail.com>
2021-11-24 14:48:37 +0000

    Auto Fixes

--- MusicKen/services/callsmusic/callsmusic.py
+++ MusicKen/services/callsmusic/callsmusic.py
@@ -17,14 +17,15 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
 
     if queues.is_empty(chat_id):
         await pytgcalls.leave_group_call(chat_id)
-    else:       
-         await pytgcalls.change_stream(
-                    queues.get(chat_id)["file"],
-                    InputStream(
-                        InputAudioStream(
-                            file,
-                        ),
-                    ),
-                )
+    else:
+        await pytgcalls.change_stream(
+            queues.get(chat_id)["file"],
+            InputStream(
+                InputAudioStream(
+                    file,
+                ),
+            ),
+        )
+
 
 run = pytgcalls.start
