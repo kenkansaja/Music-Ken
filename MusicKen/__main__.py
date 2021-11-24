9b4881b4c1ea80db45f953f71ffef600a0204972
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-11-24 20:58:31 +0700

    Update __main__.py

--- MusicKen/__main__.py
+++ MusicKen/__main__.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import Client as Bot
 from pytgcalls import idle
 
-from MusicKen.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from MusicKen.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN, SESSION_NAME
 from MusicKen.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
@@ -18,9 +18,10 @@ bot = Bot(
     plugins=dict(root="MusicKen.modules"),
 )
 
+app = (API_ID, API_HASH, SESSION_NAME)
 async def musik_on():
-    await bot.join_chat("@musikkugroup")
-    await bot.join_chat("@musikkuchannel")
+    await bot.join_chat("musikkugroup")
+    await bot.join_chat("musikkuchannel")
 
 bot.start()
 run()
