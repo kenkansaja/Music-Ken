6b8f7581ca25e41ad4e450382bcc5a494cf0f912
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-11-24 21:15:09 +0700

    Update callsmusic.py

--- MusicKen/services/callsmusic/callsmusic.py
+++ MusicKen/services/callsmusic/callsmusic.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
-from pytgcalls.types.input_stream import InputAudioStream, InputStream
-
+from pytgcalls.types.input_stream import AudioPiped
+from pytgcalls import StreamType
 from MusicKen.config import API_HASH, API_ID, SESSION_NAME
 from MusicKen.services.callsmusic.queues import queues
 
@@ -18,14 +18,9 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
     if queues.is_empty(chat_id):
         await pytgcalls.leave_group_call(chat_id)
     else:
-        await pytgcalls.change_stream(
+        await pytgcalls.join_group_call(
             chat_id,
-            InputStream(
-                InputAudioStream(
-                    queues.get(chat_id)["file"],
-                ),
-            ),
-            stream_type=StreamType().local_stream,
+                AudioPiped(queues.get(chat_id)["file"]), stream_type=StreamType().local_stream,
         )
 
 
